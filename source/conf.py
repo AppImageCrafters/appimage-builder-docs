1a6d1b6f33131a22d4c2650da5664673387e13cf
Alexis Lopez Zubieta <contact@azubieta.net>
2022-04-11 17:20:53 -0500

    Set version to 1.0.0

--- source/conf.py
+++ source/conf.py
@@ -22,7 +22,7 @@ copyright = "2022, Alexis Lopez Zubieta"
 author = "Alexis Lopez Zubieta"
 
 # The full version, including alpha/beta/rc tags
-release = "0.9.2"
+release = "1.0.0"
 
 # -- General configuration ---------------------------------------------------
 
